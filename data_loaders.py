6a98d306b6ce03eb5db2583c40ea15596dc13c9d
nvo87 <nvo87@yandex.ru>
2018-09-04 10:40:18 +0300

    main_function and calc_password_strength function are refactored.

--- data_loaders.py
+++ data_loaders.py
@@ -1,4 +1,22 @@
-def load_list_from_txt(filepath):
+def try_get_list_from_file(filepath):
+    blacklist_link = 'https://github.com/danielmiessler/' \
+                     'SecLists/tree/master/Passwords'
+    msg_about_pswrd_list = 'To check your password strength more correctly, ' \
+                           'you should find blacklist and other forbidden ' \
+                           'list by yourself. \n For example, from here {}. \n ' \
+                           'Type: -h for more info'.format(blacklist_link)
+    try:
+        return load_list_from_file(filepath)
+    except FileNotFoundError:
+        print('{} is not found or its name is wrong. \n'.format(filepath) +
+              msg_about_pswrd_list)
+        return []
+    except TypeError:
+        print(msg_about_pswrd_list)
+        return []
+
+
+def load_list_from_file(filepath):
     with open(filepath) as file_object:
         blacklist = file_object.readlines()
-    return [item.strip() for item in blacklist]
+    return [line.strip() for line in blacklist]
--- password_strength.py
+++ password_strength.py
@@ -3,7 +3,7 @@ import argparse
 import getpass
 import string
 
-from data_loaders import load_list_from_txt
+from data_loaders import load_list_from_file, try_get_list_from_file
 
 
 def parse_args():
@@ -30,10 +30,13 @@ def is_special(char):
 
 
 def calc_password_strength(password, blacklist, forbidden_list):
-    if password in blacklist:
-        return 0
+    PASSWORD_STRENGTH_MIN = 0
+    PASSWORD_STRENGTH_MAX = 10
 
-    password_strength = 10
+    if password in blacklist:
+        return PASSWORD_STRENGTH_MIN
+
+    password_strength = PASSWORD_STRENGTH_MAX
 
     upper_and_lower_chars = r'(?=[A-Z]*[a-z])(?=[a-z]*[A-Z])[a-zA-Z]'
     more_than_eight_word_length = r'\S{8,}'
@@ -47,7 +50,6 @@ def calc_password_strength(password, blacklist, forbidden_list):
     for regex in regex_list:
         if not re.search(regex, password):
             password_strength -= 2
-
     for word in forbidden_list:
         if re.search(word, password):
             password_strength -= 1
@@ -56,45 +58,18 @@ def calc_password_strength(password, blacklist, forbidden_list):
     if not spec_chars:
         password_strength -= 2
 
-    password_strength = max(0, password_strength)
-
-    return password_strength
+    return max(PASSWORD_STRENGTH_MIN, password_strength)
 
 
 if __name__ == '__main__':
-    blacklist_link = 'https://github.com/danielmiessler/' \
-                     'SecLists/tree/master/Passwords'
-    msg_about_pswrd_list = 'To check your password strength more correctly, ' \
-                           'you should find blacklist and other forbidden ' \
-                           'list by yourself. \n For example, ' \
-                           'from here {}. \n ' \
-                           'Type: -h for more info'.format(blacklist_link)
     args = parse_args()
     blacklist_filepath = args.blacklist_path
     forbidden_list_filepath = args.forbiddenlist_path
     username = input('Type your username: ')
     password = getpass.getpass(prompt='Type your password:')
 
-    try:
-        blacklist = load_list_from_txt(blacklist_filepath)
-    except FileNotFoundError:
-        print('Blacklist file is not found or its name is wrong. \n' +
-              msg_about_pswrd_list)
-        blacklist = []
-    except TypeError:
-        print(msg_about_pswrd_list)
-        forbidden_list = []
-
-    try:
-        forbidden_list = load_list_from_txt(forbidden_list_filepath)
-    except FileNotFoundError:
-        print('Forbidden-list file is not found or its name is wrong. \n' +
-              msg_about_pswrd_list)
-        forbidden_list = []
-    except TypeError:
-        print(msg_about_pswrd_list)
-        forbidden_list = []
-
+    blacklist = try_get_list_from_file(blacklist_filepath)
+    forbidden_list = try_get_list_from_file(forbidden_list_filepath)
     forbidden_list.append(username)
 
     print('password strength is:',
